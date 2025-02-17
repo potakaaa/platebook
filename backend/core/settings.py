f6f2f209a2ac6c06a67bce9f115f251dcd7cd328
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-17 20:10:46 +0800

    added new secret key

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=^yevl@@(j+fph)kss5or&0l*^j*&5*p8+3*=-9njatfa(o((4'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
