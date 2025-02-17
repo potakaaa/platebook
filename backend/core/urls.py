740c084834bc5af6445c4d1a80dcf8f0700e4773
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-17 20:22:23 +0800

    added rest auth registration urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('dj_rest_auth.urls')),
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/social/login', include('accounts.urls')),
 ]
