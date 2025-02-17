8c7b5d265d0ff42f13df10fa30d7d66dc9f12b8e
potakaaa <helbirog@gmail.com>
2025-02-17 23:14:57 +0800

    modified login and signup page

--- frontend/src/app/(authentication)/signup/page.tsx
+++ frontend/src/app/(authentication)/signup/page.tsx
@@ -1,8 +1,12 @@
 import { SignUpForm } from "@/components/authentication/signup-form";
+import HomeLogo from "@/components/navbar/nav/HomeLogo";
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted w-full p-6 md:p-10">
+      <section className="fixed top-5 left-10 m-2">
+        <HomeLogo />
+      </section>
       <div className="w-full max-w-sm md:max-w-3xl">
         <SignUpForm />
       </div>
