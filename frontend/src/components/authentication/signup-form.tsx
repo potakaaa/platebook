5473e70094506746ab816d60bf23778d4db47581
potakaaa <helbirog@gmail.com>
2025-02-20 15:40:37 +0800

    (frontend): modified signup page

--- frontend/src/components/authentication/signup-form.tsx
+++ frontend/src/components/authentication/signup-form.tsx
@@ -60,7 +60,7 @@ export function SignUpForm({
                 <Input id="confirm-password" type="password" required />
               </LabelInputContainer>
               <Button type="submit" className="w-full relative group/btn">
-                Login
+                Sign Up
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">
