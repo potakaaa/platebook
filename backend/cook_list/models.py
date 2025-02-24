16160205ce7dbbdfb315e30af29c7a963e9a107b
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-24 23:38:51 +0800

    (backend): minor cooklist change

--- backend/cook_list/models.py
+++ backend/cook_list/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from accounts.models import CustomUserModel
 from recipes.models import Recipe
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Cooklist(models.Model):
     owner = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE)
     cooklist_name = models.CharField(max_length=255)
+    cooklist_desc = models.TextField(max_length=500,blank=True, null=True)
+    cooklist_cover = CloudinaryField('cover_images', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
