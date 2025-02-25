ded8f222c605a817ecf9fe276a8ade167646ecce
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-25 11:11:57 +0800

    (backend): restructured interactions

--- backend/interactions/urls.py
+++ backend/interactions/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import LikeViewSet, ShareViewSet, CommentViewSet, FollowViewSet
+from recipes.urls import recipes_router
+
 
 router = DefaultRouter()
-router.register(r'likes', LikeViewSet, basename='like')
-router.register(r'shares', ShareViewSet, basename='share')
-router.register(r'comments', CommentViewSet, basename='comment')
+recipes_router.register(r'likes', LikeViewSet, basename='like')
+recipes_router.register(r'shares', ShareViewSet, basename='share')
+recipes_router.register(r'comments', CommentViewSet, basename='comment')
 router.register(r'follows', FollowViewSet, basename='follow')
 
 urlpatterns = [
-    path('', include(router.urls)),  # Include all interaction routes
+    path('', include(router.urls)),  
+    path('', include(recipes_router.urls)),
 ]
