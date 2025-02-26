5dd833c53dc86760bbe34a99eae8c78751c1ea00
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-26 09:27:43 +0800

    (frontend): fetchfeed service

--- frontend/src/lib/services/api/recipeServices.ts
+++ frontend/src/lib/services/api/recipeServices.ts
@@ -1,11 +1,17 @@
 import { axiosClient } from "../axiosClient";
 
-export const getRecipes = async () => {
-    try {
-        const response = await axiosClient.get("recipes/");
-        return response.data;
-    } catch (error) {
-        console.error("Error getting recipes", error);
-        return null;
+//FETCHES 15 POSTS AT A TIME, ADD INFINITE SCROLL EFFECT 
+
+const fetchFeed = async () => {
+  try {
+    const response = await axiosClient.get("/feed/");
+    return response.data;
+  } catch (error: any) {
+    console.error("Fetch Feed Error:", error.response?.data);
+    if (error.response?.data) {
+      throw error.response.data;
     }
+
+    throw new Error("An unknown error occurred.");
+  }
 };
