0f1a97a7a69387718bef13bdcda3e7f38436428b
potakaaa <helbirog@gmail.com>
2025-03-02 12:15:34 +0800

    (frontend): added loader

--- frontend/src/components/home/InfiniteScroll.tsx
+++ frontend/src/components/home/InfiniteScroll.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
 import { fetchFeed } from "@/lib/services/api/recipeServices";
 import React, { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import PostCard, { PostCardProps } from "../cards/PostCard";
 import { title } from "process";
+import Spinner from "../loader/Spinner";
 
 const InfiniteScrollComp = () => {
   const [posts, setPosts] = useState<PostCardProps[]>([]);
   const [hasMore, setHasMore] = useState(true);
   const [page, setPage] = useState<number>(1);
-
 
   useEffect(() => {
     fetchFeedData();
@@ -21,20 +21,21 @@ const InfiniteScrollComp = () => {
       const response = await fetchFeed(page);
       const data = response;
       if (data.results && Array.isArray(data.results)) {
-
-        const parsedResults: PostCardProps[] = data.results.map((result: any) => {
+        const parsedResults: PostCardProps[] = data.results.map(
+          (result: any) => {
             return {
-                userImage: result.chef.pfp_url,
-                userName: result.chef.username,
-                title: result.title,
-                description: result.description,
-                images: result.images.map((image: any) => image.image_url),
-                likeCount: result.likes,
-                shareCount: result.shares,
-                commentCount: 0,
-                atPlateList: false,
+              userImage: result.chef.pfp_url,
+              userName: result.chef.username,
+              title: result.title,
+              description: result.description,
+              images: result.images.map((image: any) => image.image_url),
+              likeCount: result.likes,
+              shareCount: result.shares,
+              commentCount: 0,
+              atPlateList: false,
             };
-            });
+          }
+        );
 
         setPosts((prevPosts) => [...prevPosts, ...parsedResults]);
 
@@ -51,7 +52,7 @@ const InfiniteScrollComp = () => {
   };
 
   const refresh = () => {
-    setPosts([]); 
+    setPosts([]);
     setPage(1);
     setHasMore(true);
     fetchFeedData();
@@ -62,20 +63,22 @@ const InfiniteScrollComp = () => {
       dataLength={posts.length}
       next={fetchFeed}
       hasMore={hasMore}
-      loader={<h4>Loading...</h4>}
+      loader={
+        <div className="w-full h-full items-center justify-center flex mt-10 overflow-hidden">
+          <Spinner />
+        </div>
+      }
       endMessage={
-        <p style={{ textAlign: "center" }}>
-          <b>Yay! You have seen it all</b>
-        </p>
+        <p className="text-center mt-20">Come back for more recipes!</p>
       }
       refreshFunction={refresh}
       pullDownToRefresh
       pullDownToRefreshThreshold={50}
       pullDownToRefreshContent={
-        <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
+        <h3 className="text-center">&#8595; Pull down to refresh</h3>
       }
       releaseToRefreshContent={
-        <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
+        <h3 className="text-center">&#8593; Release to refresh</h3>
       }
     >
       {posts.map((post, index) => (
---
+++ frontend/src/components/loader/Spinner.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="dot-spinner">
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+    </div>
+  );
+};
+
+export default Spinner;
