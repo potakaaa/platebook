efbe0c4ce36c25bfa65eebea48933f6aab43e602
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-03-04 00:16:17 +0800

    (frontend): adding terms and privacy

--- frontend/src/app/(terms)/terms-policy/page.tsx
+++ frontend/src/app/(terms)/terms-policy/page.tsx
@@ -1,7 +1,294 @@
 import React from "react";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <div className="bg-background rounded-lg font-sans max-h-[80vh] flex items-center justify-center">
+      <div className="rounded-lg p-8 w-full max-w-2xl border-2 overflow-y-auto max-h-[80vh] scroll-smooth shadow-2xl shadow-orange-400">
+        <h1 className="text-2xl font-bold text-center text-primary mb-1">
+          Terms of Service
+        </h1>
+
+        <p className="text-center text-sm mb-4">
+          Effective Date: February 24, 2025
+        </p>
+
+        <p className="text-sm mb-6">
+          Welcome to Platebook, operated by us. By accessing or using the site,
+          you agree to be bound by these Terms of Service. Please read them
+          carefully.
+        </p>
+
+        <div className="mb-6">
+          <h2
+            className="text-lg font-semibold
+                 text-primary mb-2"
+          >
+            Use of the site
+          </h2>
+
+          <p className="text-sm mb-2">
+            You may use the site only for lawful purposes and in accordance with
+            these Terms. You agree not to:
+          </p>
+
+          <ul className="list-disc list-inside text-sm">
+            <li>
+              Use the site in any way that violates any applicable federal,
+              state, local, or international law or regulation.
+            </li>
+            <li>
+              Impersonate or misrepresent your affiliation with any person or
+              entity.
+            </li>
+            <li>
+              Interfere with or disrupt the operation of the site or the servers
+              or networks connected to the site.
+            </li>
+            <li>
+              Attempt to gain unauthorized access to any part of the site or any
+              other systems or networks.
+            </li>
+            <li>
+              Use the site to transmit or distribute any viruses, worms, or
+              other malicious code.
+            </li>
+            <li>
+              Use the site to collect or harvest any information about other
+              users.
+            </li>
+            <li>
+              Use the site for any commercial purpose without our prior written
+              consent, except as specifically permitted (e.g., recipe promotion
+              with attribution).
+            </li>
+            <li>
+              Scrape or otherwise extract data from the site for any purpose
+              without our prior written consent.
+            </li>
+          </ul>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-lg font-semibold text-primary mb-2">
+            User Content
+          </h2>
+
+          <p className="text-sm mb-2">
+            You are responsible for any content you submit or post on the site
+            (User Content), including recipes, comments, photos, and videos. You
+            represent and warrant that you have all necessary rights to your
+            User Content and that your User Content does not infringe the rights
+            of any third party, including copyright, trademark, privacy, or
+            other intellectual property rights. You also represent that your
+            recipes are original or that you have proper attribution for any
+            adapted or inspired recipes.
+          </p>
+
+          <p className="text-sm mb-2">
+            By submitting User Content, you grant us a non-exclusive,
+            royalty-free, worldwide, perpetual, irrevocable license to use,
+            reproduce, modify, distribute, display, and perform your User
+            Content in connection with the site and our business, including for
+            promotional purposes. This includes the right to sublicense your
+            User Content to other users for personal, non-commercial use, such
+            as sharing on social media.
+          </p>
+
+          <p className="text-sm mb-2">
+            We have the right, but not the obligation, to monitor, remove, or
+            edit User Content at our sole discretion, including if it violates
+            these Terms, is inaccurate, or is otherwise objectionable. We may
+            also remove User Content if we receive a valid copyright
+            infringement notice.
+          </p>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-lg font-semibold  text-primary mb-2">
+            Intellectual Property
+          </h2>
+          <p className="text-sm mb-2">
+            The content on the site, excluding User Content, including text,
+            graphics, logos, images, and software, is the property of Platellook
+            or its licensors and is protected by copyright and other
+            intellectual property laws. You may not use any content from the
+            site without our prior written consent, except as specifically
+            permitted (e.g., printing recipes for personal use).
+          </p>
+        </div>
+
+        <div>
+          <h2
+            className="text-2xl font-bold
+              text-center text-primary mb-1"
+          >
+            Privacy Policy
+          </h2>
+          <p className="text-center text-sm mb-4">
+            Effective Date: February 24, 2025
+          </p>
+          <p className="mb-2 text-sm">
+            PlateBook is committed to protecting the privacy of your
+            information. This Privacy Policy explains how we collect, use, and
+            share your personal information when you visit or use our website
+            PlateBook.
+          </p>
+        </div>
+
+        <div className="mb-6">
+          <h2
+            className="text-lg font-semibold
+                 text-primary mb-2"
+          >
+            What information do we collect?
+          </h2>
+          <p className="mb-2 text-sm">
+            When you visit the site, we automatically collect certain
+            information about your device, including your IP address, web
+            browser, time zone, and some of the cookies that are installed on
+            your device.
+          </p>
+
+          <p className="text-sm mb-2">
+            When you register an account, submit a recipe, leave a comment, or
+            otherwise interact with the site, we collect information you provide
+            directly, such as:
+          </p>
+
+          <ul className="list-disc list-inside mb-2 text-sm">
+            <li>
+              Username, email address, password (hashed), and optionally, your
+              name, profile picture, and bio.
+            </li>
+            <li>
+              Recipe title, ingredients, instructions, photos, and any other
+              content you submit with your recipes.
+            </li>
+            <li>
+              Your name or username, email address (if provided), and the
+              content of your comments.
+            </li>
+            <li>
+              Information about your activity on the site, such as recipes you
+              view, save, comments you leave, and other interactions.
+            </li>
+          </ul>
+
+          <p className="text-sm mb-2">
+            We may also collect information from third-party sources, such as
+            social media platforms, if you choose to connect your accounts to
+            our site. This might include your name, profile picture, and list of
+            followers.
+          </p>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-lg font-semibold text-primary mb-2">
+            How do we use your information?
+          </h2>
+          <p className="text-sm mb-2">We use the information we collect to:</p>
+          <ul className="list-disc list-inside mb-2 text-sm">
+            <li>
+              Operate the site, personalize your experience, provide customer
+              support, and communicate with you about your account or the site.
+            </li>
+            <li>
+              Publish your recipes, comments, and other content you submit on
+              the site. This content will be visible to other users of the Site.
+            </li>
+            <li>
+              Allow you to interact with other users, such as commenting on
+              recipes, following other users, or sharing recipes on social
+              media.
+            </li>
+            <li>
+              Analyze how users interact with the site to identify areas for
+              improvement and develop new features.
+            </li>
+            <li>
+              Send you emails or notifications related to your account, new
+              recipes, site updates, or marketing communications (if you have
+              opted in).
+            </li>
+            <li>
+              Prevent fraud, spam, and other harmful activity on the site.
+            </li>
+          </ul>
+        </div>
+
+        <div className="mb-6">
+          <h2 className="text-lg font-semibold text-primary mb-2">
+            How is your information shared on PlateBook?
+          </h2>
+          <ul className="list-disc list-inside mb-2 text-sm">
+            <li>
+              Recipes, comments, and other content you submit to the site will
+              be publicly visible to other users. Your username and profile
+              picture (if you have one) may also be displayed along with your
+              content.
+            </li>
+            <li>
+              We may share your information with third-party service providers
+              who help us operate the site, such as hosting providers, email
+              marketing services, and analytics providers. We require these
+              providers to protect your information and only use it for the
+              purposes we specify.
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h2 className="text-lg font-semibold mb-2 text-primary">
+            How will you know the Policy and has changed?
+          </h2>
+          <p className="text-sm mb-2 pb-4">
+            We may update Privacy Policy and from time to time. Any changes will
+            be posted on this page with a revised effective date. We'll notify
+            you before we make material changes to this Policy.
+          </p>
+        </div>
+
+        <div>
+          <p className="text-sm mb-2">
+            If you have any questions about Terms of Use and Privacy Policy,
+            please contact us at
+            <a
+              href="mailto:platebook123@gmail.com"
+              className="pl-1 text-primary underline"
+            >
+              platebook123@gmail.com
+            </a>
+          </p>
+        </div>
+
+        <div>
+          <div className="flex items-center pt-3 pb-1">
+            <input
+              type="checkbox"
+              id="agreeTerms"
+              className="form-checkbox h-5 w-5"
+            ></input>
+            <label htmlFor="agreeTerms" className="ml-2 text-lg">
+              I agree with the Terms of Use
+            </label>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              id="agreePolicy"
+              className="form-checkbox h-5 w-5"
+            ></input>
+            <label htmlFor="agreePolicy" className="ml-2 text-lg">
+              I agree with the Privacy Policy
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
