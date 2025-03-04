dbe6f3e416ee352514f8b3c42265f7db416c8785
potakaaa <helbirog@gmail.com>
2025-03-05 02:02:33 +0800

    (frontend): added comments modal

--- frontend/src/components/cards/interaction-buttons/CommentButton.tsx
+++ frontend/src/components/cards/interaction-buttons/CommentButton.tsx
@@ -25,54 +25,71 @@ const CommentButton = ({
 }) => {
   return (
     <div>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant={"ghost"} className="">
-            <MessageCircleMore className="size-8" />
-            <span
-              className={`hidden xl:block ${
-                !forHero ? "sm:block lg:block" : "lg:hidden sm:block"
-              }`}
-            >
-              Comment
-            </span>
-          </Button>
-        </DialogTrigger>
-        <DialogContent
-          className="w-full max-w-xl rounded-xl min-h-[400px] max-h-[600px] flex flex-col justify-between overflow-y-auto pointer-events-auto"
-          style={{ overscrollBehavior: "contain" }}
-        >
-          <DialogHeader
-            className="text-left"
-            onWheel={(e) => e.stopPropagation()}
-            onTouchMove={(e) => e.stopPropagation()}
+      {!forHero ? (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant={"ghost"} className="">
+              <MessageCircleMore className="size-8" />
+              <span
+                className={`hidden xl:block ${
+                  !forHero ? "sm:block lg:block" : "lg:hidden sm:block"
+                }`}
+              >
+                Comment
+              </span>
+            </Button>
+          </DialogTrigger>
+          <DialogContent
+            className="w-full max-w-xl rounded-xl min-h-[400px] max-h-[600px] flex flex-col justify-between overflow-y-auto pointer-events-auto"
+            style={{ overscrollBehavior: "contain" }}
           >
-            <DialogTitle className="text-lg ">Comments</DialogTitle>
-            <DialogDescription>
-              <div className="flex flex-col line-clamp-2 w-full max-w-sm">
-                <p className="font-semibold text-sm">{postName}</p>
-                <p className="text-xs">by {postUser}</p>
-              </div>
-            </DialogDescription>
-          </DialogHeader>
-          <div className="w-full h-full flex flex-col space-y-5">
-            <section id="comments" className="flex flex-col space-y-3">
-              <Comments userName="Rald" userImg="" comment="lorem ipsumnida" />
-            </section>
-            <DialogFooter>
-              <div className="flex flex-row w-full gap-2">
-                <Input
-                  placeholder="Enter comment here..."
-                  className="p-3 text-sm"
-                ></Input>
-                <Button variant="ghost" size="icon">
-                  <SendHorizonal className="size-5" />
-                </Button>
-              </div>
-            </DialogFooter>
-          </div>
-        </DialogContent>
-      </Dialog>
+            <DialogHeader
+              className="text-left"
+              onWheel={(e) => e.stopPropagation()}
+              onTouchMove={(e) => e.stopPropagation()}
+            >
+              <DialogTitle className="text-lg ">Comments</DialogTitle>
+              <DialogDescription>
+                <div className="flex flex-col line-clamp-2 w-full max-w-sm">
+                  <p className="font-semibold text-sm">{postName}</p>
+                  <p className="text-xs">by {postUser}</p>
+                </div>
+              </DialogDescription>
+            </DialogHeader>
+            <div className="w-full h-full flex flex-col space-y-5">
+              <section id="comments" className="flex flex-col space-y-3">
+                <Comments
+                  userName="Rald"
+                  userImg=""
+                  comment="lorem ipsumnida"
+                />
+              </section>
+              <DialogFooter>
+                <div className="flex flex-row w-full gap-2">
+                  <Input
+                    placeholder="Enter comment here..."
+                    className="p-3 text-sm"
+                  ></Input>
+                  <Button variant="ghost" size="icon">
+                    <SendHorizonal className="size-5" />
+                  </Button>
+                </div>
+              </DialogFooter>
+            </div>
+          </DialogContent>
+        </Dialog>
+      ) : (
+        <Button variant={"ghost"} className="">
+          <MessageCircleMore className="size-8" />
+          <span
+            className={`hidden xl:block ${
+              !forHero ? "sm:block lg:block" : "lg:hidden sm:block"
+            }`}
+          >
+            Comment
+          </span>
+        </Button>
+      )}
     </div>
   );
 };
