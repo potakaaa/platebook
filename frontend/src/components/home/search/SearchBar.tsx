1bf280c086ce9570baf31de18e2e904a6f79dfb2
potakaaa <helbirog@gmail.com>
2025-03-10 21:34:54 +0800

    (frontend): search buttons now mapped

--- frontend/src/components/home/search/SearchBar.tsx
+++ frontend/src/components/home/search/SearchBar.tsx
@@ -1,14 +1,18 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Input } from "../../ui/input";
 import { ArrowRightCircleIcon, SearchIcon } from "lucide-react";
 import useSearchStore from "@/store/search/useSearchStore";
 import { useDebounce } from "@/hooks/useDebounce";
+import { useFocusStore } from "@/store/focus/useFocusStore";
 
 const SearchBar = () => {
   const { searchQuery, setSearchQuery } = useSearchStore();
   const [inputValue, setInputValue] = useState(searchQuery);
   const debouncedValue = useDebounce(inputValue, 500);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { focusComponentId, setFocusComponentId } = useFocusStore();
 
   useEffect(() => {
     if (debouncedValue === "" || debouncedValue === null) {
@@ -22,6 +26,21 @@ const SearchBar = () => {
     setInputValue(e.target.value);
   };
 
+  useEffect(() => {
+    if (focusComponentId === "search-home" && inputRef.current) {
+      // Scroll smoothly first
+      inputRef.current.scrollIntoView({ behavior: "smooth", block: "center" });
+
+      // Delay the focus slightly to avoid abrupt jumps
+      setTimeout(() => {
+        inputRef.current?.focus();
+      }, 300); // Adjust delay as needed
+
+      // Reset focus state after a short delay
+      setTimeout(() => setFocusComponentId(null), 500);
+    }
+  }, [focusComponentId, setFocusComponentId]);
+
   return (
     <div className="w-full flex items-center justify-center mt-1 sm:mt-0">
       <form className="w-full flex flex-row border border-primary rounded-full px-2 items-center justify-center">
@@ -29,6 +48,7 @@ const SearchBar = () => {
           type="search"
           onChange={handleChange}
           value={inputValue}
+          ref={inputRef}
           placeholder="Search for recipes"
           className="h-8 sm:h-10 text-xs sm:text-sm w-full border-none text-ellipsis focus-visible:ring-none focus-visible:ring-0"
         />
