a4aa50b958f399d1f9ca8239345fed5dd9ee366b
hdmGOAT <hdmGOAT@users.noreply.github.com>
2025-03-10 14:01:23 +0800

    fixed redirect

--- frontend/src/components/authentication/oauth-buttons.tsx
+++ frontend/src/components/authentication/oauth-buttons.tsx
@@ -11,15 +11,36 @@ import {
 import { BottomGradient } from "../ui/bottom-gradient";
 import { signIn } from "next-auth/react";
 import { sign } from "crypto";
+import { useRouter } from "next/router";
 
 const OauthButtons = () => {
+
+  const router = useRouter();
+
+  const handleSignIn = async (provider: string) => {
+    try {
+      const response = await signIn(provider, {
+        callbackUrl: "/home",
+        redirect: false, 
+      });
+
+      if (response?.ok) {
+        router.push("/home"); 
+      } else {
+        console.error("Login failed:", response?.error);
+      }
+    } catch (error) {
+      console.error("Unexpected error:", error);
+    }
+  };
+
   return (
     <div className="grid grid-cols-2 gap-4">
       <Button
         variant="outline"
         className="w-full relative group/btn dark:bg-transparent"
         type="button"
-        onClick={() => signIn("discord", { callbackUrl: "/home" })}
+        onClick={() => handleSignIn("discord")}
       >
         <IconBrandDiscordFilled size={24} />
         <span className="sr-only">Login with Discord</span>
@@ -29,7 +50,7 @@ const OauthButtons = () => {
         type="button"
         variant="outline"
         className="w-full relative group/btn dark:bg-transparent"
-        onClick={() => signIn("google", { callbackUrl: "/home" })}
+        onClick={() => handleSignIn("google")}
       >
         <IconBrandGoogleFilled size={24} />
         <span className="sr-only">Login with Google</span>
