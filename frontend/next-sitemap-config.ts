bdc2c2c568150e519a764fe747f03dae696a9829
potakaaa <helbirog@gmail.com>
2025-03-11 19:59:53 +0800

    added sitemap for dynamic seo

--- frontend/next-sitemap-config.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-const dev = process.env.NODE_ENV !== "production";
-
-module.exports = {
-  siteUrl: dev ? "http://127.0.0.1:3000" : "https://platebook.vercel.app",
-};
