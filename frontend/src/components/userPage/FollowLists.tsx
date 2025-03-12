2f3482ba03782e2ad47119a37669617166b38d41
potakaaa <helbirog@gmail.com>
2025-03-12 23:17:50 +0800

    (frontend): modifying following list

--- frontend/src/components/userPage/FollowLists.tsx
+++ frontend/src/components/userPage/FollowLists.tsx
@@ -38,71 +38,95 @@ import { storedUser, useUserStore } from "@/store/user/UserStore";
 import useQueryInteraction from "@/hooks/tanstack/interaction/useQueryInteractions";
 import CustomAvatar from "../user/CustomAvatar";
 import { useRouter } from "next/navigation";
+import { ScrollArea } from "../ui/scroll-area";
+import useMutationInteraction from "@/hooks/tanstack/interaction/useMutationInteraction";
 
-interface FollowListsProps{
-  userId: string,
-  count: number,
+interface FollowListsProps {
+  userId: string;
+  count: number;
   type: "Followers" | "Following";
 }
 
-
 type simpleUser = {
-  pfp_url: string,
-  userId: string,
-  username: string
-}
-const FollowLists:React.FC<FollowListsProps> = ({userId, type, count = 0}) => {
-  
+  pfp_url: string;
+  userId: string;
+  username: string;
+};
+const FollowLists: React.FC<FollowListsProps> = ({
+  userId,
+  type,
+  count = 0,
+}) => {
   const [open, setOpen] = useState(false);
-  const [list, setList]= useState<simpleUser[]>([]);
+  const [list, setList] = useState<simpleUser[]>([]);
 
-  const {useQueryGetFollowers, useQueryGetFollowing} = useQueryInteraction();
+  const { useQueryGetFollowers, useQueryGetFollowing } = useQueryInteraction();
+  const { useMutationFollowUser, useMutationUnfollowUser } =
+    useMutationInteraction();
 
-  const {data: followersData, isLoading: followersLoading} = useQueryGetFollowers(userId);
-  const {data: followingData, isLoading: followingLoading} = useQueryGetFollowing(userId);
+  const { mutate: follow, isPending: FollowPending } = useMutationFollowUser();
+  const { mutate: unfollow, isPending: UnfollowPending } =
+    useMutationUnfollowUser();
+
+  const { data: followersData, isLoading: followersLoading } =
+    useQueryGetFollowers(userId);
+  const { data: followingData, isLoading: followingLoading } =
+    useQueryGetFollowing(userId);
 
   const router = useRouter();
 
   useEffect(() => {
     if (type === "Following" && followingData) {
-      setList(followingData.map((user: any)=> user.followed_user) || []); 
+      setList(followingData.map((user: any) => user.followed_user) || []);
     } else if (type === "Followers" && followersData) {
-      setList(followersData.map((user: any)=> user.user) || []); 
+      setList(followersData.map((user: any) => user.user) || []);
     }
   }, [followingData, followersData, type]);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <div>
-          <p className="text-center font-bold sm:text-lg">
-              {count}
-          </p>
+        <div className="cursor-pointer">
+          <p className="text-center font-bold sm:text-lg">{count}</p>
           <h1 className="font-medium text-xs sm:text-sm">{type}</h1>
         </div>
       </DialogTrigger>
-      <DialogContent
-        className="w-5/6 min-h-[100px] [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300
-      dark:[&::-webkit-scrollbar-track]:bg-neutral-700
-      dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 [&::-webkit-scrollbar]:w-2 gap-2 rounded-xl overflow-x-hidden overflow-y-auto flex flex-col items-center justify-center"
-      >
-        <DialogTitle className="text-center font-bold">
-          {type}
-        </DialogTitle>
-        {list !== undefined && list.map((item: simpleUser)=>(
-           <div className="w-full flex flex-row space-x-2 items-center" key={item.userId}>
+      <DialogContent className="w-5/6 min-h-[100px] gap-2 rounded-xl overflow-x-hidden overflow-y-auto flex flex-col items-center justify-center">
+        <DialogTitle className="text-center font-bold">{type}</DialogTitle>
+        <span className="w-full border-b border-muted" />
+        <ScrollArea className="h-[300px] w-full">
+          <div className="flex flex-col space-y-3 w-full">
+            {list !== undefined &&
+              list.map((item: simpleUser) => (
+                <>
+                  <div
+                    className="w-full flex flex-row space-x-2 items-center justify-between pr-5"
+                    key={item.userId}
+                  >
+                    <section className="w-full flex flex-row space-x-2 items-center">
                       <CustomAvatar
                         userName={item.username}
                         userImage={item.pfp_url}
-                        onClick={()=>router.push(`/home/user/${item.userId}`)}
+                        onClick={() => router.push(`/home/user/${item.userId}`)}
                       />
                       <p className="text-xs sm:text-sm lg:text-base font-semibold">
                         {item.username}
                       </p>
-                    </div>
-        ))}
+                    </section>
+                    <Button
+                      variant={"outline"}
+                      className="text-xs sm:text-sm p-2 py-1 sm:p-3"
+                      onClick={() => {}}
+                    >
+                      Follow
+                    </Button>
+                  </div>
+                  <span className="w-full border-b border-muted" />
+                </>
+              ))}
+          </div>
+        </ScrollArea>
         <span className="border-b border-muted mb-2" />
-     
       </DialogContent>
     </Dialog>
   );
